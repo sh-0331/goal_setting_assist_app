d92b0056fbfc299a64c288a09a0154f36c2529f5
sh-0331 <syoshimuraayh@gmail.com>
2023-05-02 11:26:34 +0900

    Mod: ルーティング設定

--- goal-setting-assist-app/routes/web.php
+++ goal-setting-assist-app/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\GoalController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [GoalController::class, 'index']);
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('/goals', GoalController::class);
