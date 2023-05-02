df7e8279ff3d9fe7d9133fdd0d6932d2cea49c42
sh-0331 <syoshimuraayh@gmail.com>
2023-05-02 11:51:47 +0900

    Upd: ログイン状態が切れたらログイン画面へリダイレクト

--- goal-setting-assist-app/routes/web.php
+++ goal-setting-assist-app/routes/web.php
@@ -15,10 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [GoalController::class, 'index']);
+Route::get('/', [GoalController::class, 'index'])->middleware('auth');
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::resource('/goals', GoalController::class);
+Route::resource('/goals', GoalController::class)->middleware('auth');
