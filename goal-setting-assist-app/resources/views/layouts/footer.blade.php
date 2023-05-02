d79c772ca02fb70445e67f3ce13ef7f297555e1e
sh-0331 <syoshimuraayh@gmail.com>
2023-05-02 10:35:42 +0900

    Mod: layoutsヘッダー&フッター部品化

---
+++ goal-setting-assist-app/resources/views/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+<footer>
+  <div class="d-flex justify-content-center align-items-center bg-white fixed-bottom" style="height: 60px; z-index: auto;">
+    <a class="text-decoration-none link-dark" href="{{ url('/') }}">{{ config('app.name', 'Laravel') }}</a>
+  </div>
+</footer>
