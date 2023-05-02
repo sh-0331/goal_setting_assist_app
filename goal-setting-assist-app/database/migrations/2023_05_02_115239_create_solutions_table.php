aecfdebfe9f4decf9db765d8e8dcd97b568f4560
sh-0331 <syoshimuraayh@gmail.com>
2023-05-02 22:17:18 +0900

    New: Solutionモデル&マイグレーション作成

---
+++ goal-setting-assist-app/database/migrations/2023_05_02_115239_create_solutions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('solutions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('goals_id')->constrained()->cascadeOnDelete();
+            $table->text('content');
+            $table->integer('eval');
+            $table->boolean('done')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('solutions');
+    }
+};
