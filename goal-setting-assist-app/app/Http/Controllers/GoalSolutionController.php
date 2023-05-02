b2162cbcef44dc3f2ecd91cc0cc44e7b775e8f15
sh-0331 <syoshimuraayh@gmail.com>
2023-05-02 22:18:36 +0900

    New: GoalSolutionコントローラ作成

---
+++ goal-setting-assist-app/app/Http/Controllers/GoalSolutionController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Solution;
+use Illuminate\Http\Request;
+
+class GoalSolutionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, $goal)
+    {
+        $solution = new Solution();
+        // dd($goal);
+        $solution->goals_id = $goal;
+        $solution->content = $request->input('content');
+        $solution->eval = $request->input('eval');
+        $solution->save();
+
+        return redirect()->route('goals.show', compact('goal'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Solution  $solution
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Solution $solution)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Solution  $solution
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Solution $solution)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Solution  $solution
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Solution $solution)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Solution  $solution
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Solution $solution)
+    {
+        //
+    }
+}
