17253cf8bba50902ab9c93b737bd69773072f384
sh-0331 <syoshimuraayh@gmail.com>
2023-05-03 17:14:46 +0900

    New: Milestoneモデル作成

---
+++ goal-setting-assist-app/app/Models/Milestone.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Milestone extends Model
+{
+    use HasFactory;
+
+    public function solution()
+    {
+        return $this->belongsTo(Solution::class);
+    }
+}
---
+++ goal-setting-assist-app/database/migrations/2023_05_03_072440_create_milestones_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('milestones', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('solution_id')->constrained()->cascadeOnDelete();
+            $table->text('content');
+            $table->integer('date');
+            $table->integer('rank');
+            $table->boolean('done')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('milestones');
+    }
+};
